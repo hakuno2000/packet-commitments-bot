d70c44521f3b6c82685d8f307f9e187d9d0a5513
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2025-01-16 01:56:52 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,19 @@
+FINAL_RESULT=""
+readarray -t chains < <(cat channels.json | jq -c '.[]')
+for chain in "${chains[@]}"; do
+  chain_id=$(echo "$chain" | jq -r '.chain')
+  channellist=$(echo "$chain" | jq -c '.channels')
+  #echo "$chain_id"
+    readarray -t channels < <(echo $channellist | jq -c '.[]')
+    for channel in "${channels[@]}"; do
+      channel="${channel%\"}"
+      channel="${channel#\"}"
+      #echo "$channel"
+      QUERY="$(bash hermes_query.sh $chain_id $channel)"
+      if [ "$QUERY" != "" ]; then
+        FINAL_RESULT+="$QUERY"
+        FINAL_RESULT+=$'\n'
+      fi
+    done
+done
+echo "$FINAL_RESULT"
